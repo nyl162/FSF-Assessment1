6d9b4a34dd505eb70b84805326e72b0a65a9c928
NGYL <nyl162@hotmail.com>
2018-09-07 15:44:14 +0800

    .

--- src/app/services/registrated.service.ts
+++ src/app/services/registrated.service.ts
@@ -3,15 +3,38 @@ import {InputI} from '../models/models';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class RegistratedService {
+
+  /*
+private handleError(error: HttpErrorResponse) {
+  if (error.error instanceof ErrorEvent) {
+    console.error('An error occurred:', error.error.message);
+  } else {
+    console.error(
+      `Backend returned code ${error.status}, ` +
+      `body was: ${error.error}`);
+  }
+  return throwError(
+    'Something bad happened; please try again later.');
+};
+  */
 
   constructor(private http: HttpClient) { }
 
-  AddRegistration(n:InputI){
-    return this.http.post(`${environment.api_url}submit`, n);
+  AddRegistration(n:InputI):Observable<InputI>{
+    return this.http.post<InputI>(`${environment.api_url}submit`, n);
+    /*.pipe(
+      catchError(this.handleError(' AddRegistration',n))
+    );*/
   }
 
+
+  
 }
